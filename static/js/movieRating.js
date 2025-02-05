ceecc807967cd5dfced7082e4349c92768cfc781
nilsishome <nilsnyberg24@gmail.com>
2025-02-05 17:25:34 +0100

    Add golden star rating to movie-page, based on average movie rating + hover text

--- static/js/movieRating.js
+++ static/js/movieRating.js
@@ -1,10 +1,13 @@
 async function fetchMovieRating(id) {
+  const emptyStar = document.querySelector(".singleMovie__rating_star-empty");
+  const filledStar = document.querySelector(".singleMovie__rating_star-filled");
+
   const resp = await fetch(`/movies/${id}/average-rating`);
   if (resp.ok) {
     const payload = await resp.json();
     const averageRating = payload.data;
-    const ratingText = document.querySelector(".singleMovie_rating");
-    ratingText.innerText = averageRating;
+    emptyStar.setAttribute("title", `Betyg: ${averageRating} av 5.`);
+    filledStar.style.width = averageRating * 20 + "%";
   } else {
     console.error("ERROR: Average rating could not be found.");
   }
