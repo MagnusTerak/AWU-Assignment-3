e9fa8cf5afbe2087f36e25e850862a583600f4a5
Taru Keskinen <taru.keskinen@lerniastudent.se>
2025-02-06 12:36:44 +0100

    test and logic for screenings 5 days from now

--- src/screenings.js
+++ src/screenings.js
@@ -2,9 +2,20 @@
 export default async function getScreenings(cmsAdapter) {
     const screenings = await cmsAdapter.loadScreenings();
 
-
-
-    //Logik hit
-    return screenings;
+      const today = new Date();  
+      const inFiveDays = new Date(); 
+      inFiveDays.setDate(today.getDate() + 5);  
+  
+     
+      const filteredScreenings = screenings.filter((screening) => {
+          const screeningDate = new Date(screening.attributes.start_time);
+            console.log(screeningDate);
+          return screeningDate >= today && screeningDate <= inFiveDays;
+      });
+  
+      return filteredScreenings;
+  
 }
+
+
  
--- tests/screenings.test.js
+++ tests/screenings.test.js
@@ -2,22 +2,45 @@ import { describe, expect, it, jest } from '@jest/globals';
 import getScreenings from '../src/screenings';
 
 
-/* test för hämtning av screenings som ska vara tom */
 describe('getScreenings()', () => {
-it ('returns empty array', async () => {
-    const fakeCmsAdapter = {
-        loadScreenings: async () => [],     
+
+  
+    // Testar att det bara är visningar för de fem kommande dagar som syns
+    it('only returns screenings for the next five days', async () => {
+      jest.useFakeTimers();
+      jest.setSystemTime(new Date(2025, 1, 7)); 
+  
+  
+      const fakeCmsAdapter = {
+        loadScreenings: async () => [ 
+          mockScreening({ start_time: "2025-02-07" }),
+          mockScreening({ start_time: "2025-02-08" }),
+          mockScreening({ start_time: "2025-02-09" }),
+          mockScreening({ start_time: "2025-02-10" }),
+          mockScreening({ start_time: "2025-02-11" }),
+          mockScreening({ start_time: "2025-02-12" }),
+          mockScreening({ start_time: "2025-02-13" }),
+        ],
+      }
+  
+      const screenings = await getScreenings(fakeCmsAdapter);
+      expect(screenings).toHaveLength(5);
+    });
+  });
+  
+  
+  function mockScreening(overrides) {
+    return {
+      id: 1,
+      attributes: {
+        start_time: "2025-02-07T00:00:00.000Z", 
+        createdAt: "2025-02-05T07:36:08.919Z",
+        updatedAt: "2025-02-05T07:36:08.919Z",
+        ...overrides, 
+      },
     }
+  }
 
-const screenings = await getScreenings(fakeCmsAdapter);
-expect(screenings).toHaveLength(0);
-
-
-});
-}); 
-
-
-// Test för fem kommande dagar
 
 // test för 10 visningar max, om det finns fler så ska det bara visas de dagar som 10 visningar 
 
