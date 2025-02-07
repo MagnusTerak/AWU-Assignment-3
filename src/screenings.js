34876352ba4b393abe1de96bad9b6abaf854766d
Taru Keskinen <taru.keskinen@lerniastudent.se>
2025-02-07 12:17:33 +0100

    test and logic for 10 screenings total

--- src/screenings.js
+++ src/screenings.js
@@ -2,21 +2,22 @@
 export default async function getScreenings(cmsAdapter) {
     const screenings = await cmsAdapter.loadScreenings();
 
-      const today = new Date();  
-      const inFiveDays = new Date(); 
-      inFiveDays.setDate(today.getDate() + 4);  
-  
-     
-      const filteredScreenings = screenings.filter((screening) => {
-          const screeningDate = new Date(screening.attributes.start_time);
-            
-          return screeningDate >= today && screeningDate <= inFiveDays;
-      });
-      
+    const today = new Date();  
+    const inFiveDays = new Date(); 
+    inFiveDays.setDate(today.getDate() + 5);
 
+    
+    const filteredScreenings = screenings.filter((screening) => {
+        const screeningDate = new Date(screening.attributes.start_time);
+        return screeningDate >= today && screeningDate <= inFiveDays;
+    });
 
-      return filteredScreenings;
+    
+    const limitedScreenings = filteredScreenings.slice(0, 10);
+
+    return limitedScreenings;
 }
+
 
 
  
--- tests/screenings.test.js
+++ tests/screenings.test.js
@@ -28,30 +28,39 @@ describe('getScreenings()', () => {
     });
 
 
-
     /* Max 10 filmvisningar ska visas. 
     Om det finns fler än 10 visningar de kommande 
     fem dagarna ska bara så många dagar visas som resulterar i max 10 visningar. */
-    it('', async () => {
-        
-    
-    
-        const fakeCmsAdapter = {
-          loadScreenings: async () => [ 
-            mockScreening({ start_time: "2025-02-07" }),
-            mockScreening({ start_time: "2025-02-08" }),
-            mockScreening({ start_time: "2025-02-09" }),
-            mockScreening({ start_time: "2025-02-10" }),
-            mockScreening({ start_time: "2025-02-11" }),
-            mockScreening({ start_time: "2025-02-12" }),
-            mockScreening({ start_time: "2025-02-13" }),
-          ],
-        }
-    
-        const screenings = await getScreenings(fakeCmsAdapter);
-        
-      });
-    
+    it('only returns up to 10 screenings over the next 5 days', async () => {
+      jest.useFakeTimers();
+      jest.setSystemTime(new Date(2025, 1, 7)); 
+  
+      const fakeCmsAdapter = {
+        loadScreenings: async () => [
+          mockScreening({ start_time: "2025-02-07" }),
+          mockScreening({ start_time: "2025-02-08" }),
+          mockScreening({ start_time: "2025-02-09" }),
+          mockScreening({ start_time: "2025-02-10" }),
+          mockScreening({ start_time: "2025-02-11" }),
+          mockScreening({ start_time: "2025-02-12" }),
+          mockScreening({ start_time: "2025-02-13" }),
+          mockScreening({ start_time: "2025-02-07" }),
+          mockScreening({ start_time: "2025-02-08" }),
+          mockScreening({ start_time: "2025-02-09" }),
+          mockScreening({ start_time: "2025-02-10" }),
+          mockScreening({ start_time: "2025-02-11" }),
+          mockScreening({ start_time: "2025-02-12" }),
+          mockScreening({ start_time: "2025-02-13" }),
+          mockScreening({ start_time: "2025-02-07" }),
+          mockScreening({ start_time: "2025-02-08" }),
+          mockScreening({ start_time: "2025-02-09" }),
+          mockScreening({ start_time: "2025-02-10" }),
+        ],
+      }
+  
+      const screenings = await getScreenings(fakeCmsAdapter);
+      expect(screenings).toHaveLength(10); 
+    });
 
   });
   
