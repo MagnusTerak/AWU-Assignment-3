6f437fc09c784d0b869d7c57502d8ce0ed6ea475
Taru Keskinen <taru.keskinen@lerniastudent.se>
2025-02-07 14:04:36 +0100

    change classnames

--- static/js/upcomingScreenings.js
+++ static/js/upcomingScreenings.js
@@ -6,30 +6,30 @@ async function fetchScreenings() {
     }
     const responseData = await response.json();
     
-    const screeningsList = document.querySelector('.screeningsList');
+    const screeningsList = document.querySelector('.upcoming-screenings__list');
     
-      responseData.data.forEach(screening => {
+    responseData.data.forEach(screening => {
       const movieTitle = screening.attributes.movie.data.attributes.title;
       const movieImage = screening.attributes.movie.data.attributes.image.url;
       const startTime = new Date(screening.attributes.start_time).toLocaleString();
       
       const movieCard = document.createElement('div');
-      movieCard.classList.add('movieCard');
+      movieCard.classList.add('movie-card');
       
       const imageElement = document.createElement('img');
       imageElement.src = movieImage;
       imageElement.alt = movieTitle;
-      imageElement.classList.add('movieScreeningImage');
+      imageElement.classList.add('movie-card__image');
       movieCard.appendChild(imageElement);
       
       const titleElement = document.createElement('span');
       titleElement.textContent = movieTitle;
-      titleElement.classList.add('movieScreeningTitle');
+      titleElement.classList.add('movie-card__title');
       movieCard.appendChild(titleElement);
       
       const timeElement = document.createElement('span');
       timeElement.textContent = startTime;
-      timeElement.classList.add('movieScreeningTime');
+      timeElement.classList.add('movie-card__time');
       movieCard.appendChild(timeElement);
       
       screeningsList.appendChild(movieCard);
